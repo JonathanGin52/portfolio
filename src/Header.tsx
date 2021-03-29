57968a75fcc16e181c4ea6fd6db7293d6a5adfa7
Jonathan Gin <jonathangin52@gmail.com>
2021-03-29 00:08:34 -0400

    Apply theme to header section

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,45 +6,78 @@ import {
   faLinkedin,
   faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
-import jonathan from './assets/jonathan.jpg';
+
+import {jonathan} from './assets';
 import './styles/Header.css';
 
 const Header = () => {
   return (
-    <header className="Header">
+    <header id="Header">
       <div className="ImageContainer">
         <img src={jonathan} className="Image" alt="Jonathan Gin" />
       </div>
       <hr />
-      <div>
+      <article>
         <p className="Summary">
-          Hi, I'm Jonathan. I'm currently working as a software developer at Shopify.
-          Visit me at some at some of the platforms below, or email me
-          at <a href="mailto:hello@jonathangin.me">hello@jonathangin.me</a>.
+          Hi, I&apos;m Jonathan. I&apos;m currently working as a software
+          developer at Shopify. Visit me at some at some of the platforms below,
+          or email me at{' '}
+          <a
+            id="email-link"
+            href="mailto:hello@jonathangin.me"
+            target="_blank"
+            rel="noopener"
+            aria-label="Email"
+          >
+            <span data-content="hello@jonathangin.me">
+              hello@jonathangin.me
+            </span>
+          </a>
+          {/* */}.
         </p>
-        <div className="SocialLinks">
-          <span className="Social">
-            <a href="https://www.github.com/JonathanGin52" target="_blank" rel="noreferrer" aria-label="Instagram">
+        <ul className="SocialLinks">
+          <li className="Social">
+            <a
+              href="https://www.github.com/JonathanGin52"
+              target="_blank"
+              rel="noopener"
+              aria-label="Instagram"
+            >
               <FontAwesomeIcon icon={faGithub} />
             </a>
-          </span>
-          <span className="Social">
-            <a href="https://www.linkedin.com/in/jonathangin" target="_blank" rel="noreferrer" aria-label="Instagram">
+          </li>
+          <li className="Social">
+            <a
+              href="https://www.linkedin.com/in/jonathangin"
+              target="_blank"
+              rel="noopener"
+              aria-label="Instagram"
+            >
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
-          </span>
-          <span className="Social">
-            <a href="https://twitter.com/jonathangin52" target="_blank" rel="noreferrer" aria-label="Instagram">
+          </li>
+          <li className="Social">
+            <a
+              href="https://twitter.com/jonathangin52"
+              target="_blank"
+              rel="noopener"
+              aria-label="Instagram"
+            >
               <FontAwesomeIcon icon={faTwitter} />
             </a>
-          </span>
-          <span className="Social">
-            <a href="https://www.instagram.com/jonathangin52/" target="_blank" rel="noreferrer" aria-label="Instagram">
+          </li>
+          <li className="Social">
+            <a
+              href="https://www.instagram.com/jonathangin52"
+              target="_blank"
+              rel="noopener"
+              aria-label="Instagram"
+            >
               <FontAwesomeIcon icon={faInstagram} />
             </a>
-          </span>
-        </div>
-      </div>
+          </li>
+        </ul>
+      </article>
     </header>
   );
 };
